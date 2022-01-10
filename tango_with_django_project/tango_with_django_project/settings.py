24e1c825d539b1dfdb35f96e5b57b556d1a12867
Lei <1357492993@qq.com>
2022-01-10 15:11:57 +0800

    Created project and app, finished chapter 3

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@-s$g_$#gl^k)&n$&tox0b^xr5-qknzew@(-c&-tv@+fivrq!#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
