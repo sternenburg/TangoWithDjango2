5a3135c73e2985bd1f56649dd0e49ea286d8b593
Lei <1357492993@qq.com>
2022-01-11 16:53:43 +0800

    setup and create templates, finish Chap 4   clear exit

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rango import views
 
+
+
 app_name = 'rango'
 
 urlpatterns = [
--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -66,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media', 
             ],
         },
     },
@@ -117,6 +120,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
@@ -127,3 +132,5 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # The STATIC_URL variable as the location with which clients
 # can access static content.
 STATIC_URL = '/static/'
+
+
