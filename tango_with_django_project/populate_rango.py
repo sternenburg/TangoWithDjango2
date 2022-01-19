4be9a6724290d30d609c089a6328ee9dd3eebb34
Lei <1357492993@qq.com>
2022-01-19 15:35:33 +0800

    finished the exercises in Chap 5

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -31,14 +31,24 @@ def populate():
         {'title': 'Bing',
          'url': 'http://www.bing.com'},
     ]
-    cats = {'Python': {'pages': python_pages},
-            'Movie': {'pages': movie_pages},
-            'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 
+                       'views': 128,
+                       'likes':64
+                       },
+            'Movie': {'pages': movie_pages,
+                      'views': 64,
+                      'likes': 32
+                      },
+            'Other Frameworks': {'pages': other_pages,
+                                 'views': 32,
+                                 'likes': 16
+                                 }
+            }
     
     # Goes through the cats dictionary, then adds each c
     # and then adds all the associated pages for that category
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
             
@@ -56,8 +66,10 @@ def add_page(cat, title, url, views = 0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
